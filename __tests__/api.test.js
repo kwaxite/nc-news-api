bd17b0edad967b5db89b1cebda66226d6b0992c8
kwaxite <146014461+kwaxite@users.noreply.github.com>
2024-02-19 22:50:27 +0000

    Task 3 - Create GET /api for endpoints file

--- __tests__/api.test.js
+++ __tests__/api.test.js
@@ -4,13 +4,10 @@ const seed = require("../db/seeds/seed")
 const testData = require("../db/data/test-data/index")
 const db = require("../db/connection");
 const { selectAllTopics } = require("../models/topics.model");
+const endPointsJson=require("../endpoints.json") 
 
-beforeEach(() => {
-    return seed(testData);
-    });
-    afterAll(() => {
-    db.end();
-    });
+beforeEach(() => {return seed(testData);});
+afterAll(() => {db.end();});
 
 describe("Topics", ()=>{
     describe("GET /api/topics", ()=>{
@@ -22,11 +19,22 @@ describe("Topics", ()=>{
             .get("/api/topics")
             .expect(200)
             .then((response) => {
-                console.log("test", response.body)
                 const topics = response.body.topics;
                 expect(topics.length).toBe(3);
                 expect(Array.isArray(topics)).toBe(true);
             })
+        })
+    })
+})
+
+describe("GET /api", ()=>{
+    test ("responds with object describing all the available endpoints on your APi", ()=>{
+        return request(app).get("/api").expect(200)
+        
+        .then((response) =>{
+            
+            const appJsonData = response.body
+            expect(appJsonData).toEqual(endPointsJson)
         })
     })
 })
