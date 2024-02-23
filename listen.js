eb6c8efaa30d0cdf900a16600d06f389503f162b
kwaxite <146014461+kwaxite@users.noreply.github.com>
2024-02-23 21:57:56 +0000

    Update connection pool

--- listen.js
+++ listen.js
@@ -1,5 +1,10 @@
-const app = require('./app')
+// const app = require('./app')
 
-app.listen(9090, ()=>{
-    ("App listening on port: 9090")
-})
+// app.listen(9090, ()=>{
+//     ("App listening on port: 9090")
+// })
+
+const app = require('./app.js');
+const { PORT = 9090 } = process.env;
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
